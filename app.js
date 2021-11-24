9325a258f9f7374c0dd6fc1fb3306d923b2fb5cf
Hamza Azouzi <hahi18025@gmail.com>
2021-11-24 16:02:08 +0100

    second commit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ let Students = [];
 let year = "";
 let month = "";
 let day = "";
+let final = [["order", "name", "subject", "date"]];
 
 function save() {
   Students.push({ fullName: names.value, subject: subject.value, time: "" });
@@ -24,6 +25,7 @@ function save() {
 }
 
 const DisplayListStudens = (Students) => {
+  final = [["order", "name", "subject", "date"]];
   let divFullNmae = document.createElement("div");
   let divSubject = document.createElement("div");
   let divTime = document.createElement("div");
@@ -38,7 +40,7 @@ const DisplayListStudens = (Students) => {
 
   divTime.innerHTML = "Time";
   divTime.classList.add("time");
-  Students.map((e) => {
+  Students.map((e, idx) => {
     divFullNmae.innerHTML += `
             <span>${e.fullName}</span>
         `;
@@ -50,23 +52,49 @@ const DisplayListStudens = (Students) => {
     divTime.innerHTML += `
         <span>${e.time ? e.time : "Pending"}</span>
         `;
+    final.push([idx + 1, e.fullName, e.subject, e.time ? e.time : "Pending"]);
   });
-
+  console.log(final);
   fullName.appendChild(divFullNmae);
   Subject.appendChild(divSubject);
   Time.appendChild(divTime);
+};
+
+const downloadCSV = () => {
+  let csvContent =
+    "data:text/csv;charset=utf-8," + final.map((e) => e.join(",")).join("\n");
+  var encodedUri = encodeURI(csvContent);
+  window.open(encodedUri);
 };
 
 list.addEventListener("click", () => {
   DisplayListStudens(Students);
 });
 
+const skipWeekend = (d, days) => {
+  date = new Date(d); // use a clone
+  if (days == 0 && !isNotWeekEnd(date)) {
+    return moment(defaultSkipWeekEnd(d));
+  }
+  while (days > 0) {
+    date = date.add(1, "days");
+    if (isNotWeekEnd(date)) {
+      days -= 1;
+    }
+  }
+  return date;
+};
+
 Tourage.addEventListener("click", () => {
   let cuurDate = new Date(DateInput.value);
   Students.forEach((elm, idx) => {
     let studentData = elm;
-    console.log('ana hna ',cuurDate.getDay() );
-    if (cuurDate.getDay() == 6) {
+    console.log("ana hna ", cuurDate.getDay());
+    if (cuurDate.getDay() == 5) {
+      studentData.time = new Date(
+        cuurDate.setDate(cuurDate.getDate() + 3)
+      ).toLocaleDateString();
+    } else if (cuurDate.getDay() == 6) {
       studentData.time = new Date(
         cuurDate.setDate(cuurDate.getDate() + 2)
       ).toLocaleDateString();
@@ -76,7 +104,7 @@ Tourage.addEventListener("click", () => {
       ).toLocaleDateString();
     } else {
       studentData.time = new Date(
-        cuurDate.setDate(cuurDate.getDate() + (idx === 0 ? 0 : 1))
+        cuurDate.setDate(new Date(cuurDate.getDate() + (idx === 0 ? 0 : 1)))
       ).toLocaleDateString();
     }
 
@@ -84,11 +112,7 @@ Tourage.addEventListener("click", () => {
     console.log(studentData);
     DisplayListStudens(Students);
   });
-
-
 });
-
-
 
 function randomDate(start, end) {
   return new Date(
@@ -99,3 +123,4 @@ function randomDate(start, end) {
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
